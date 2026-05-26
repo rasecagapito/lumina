6f9a0a6a7ff9872c24bd9247ef1ad2f70b3aa233
rasecagapito <cesarragapito@gmail.com>
2026-05-26 20:24:35 -0300

    feat: add server entrypoint for local dev

---
+++ apps/api/src/server.ts
@@ -0,0 +1,7 @@
+import { serve } from '@hono/node-server'
+import app from './index'
+
+const port = Number(process.env.PORT ?? 3001)
+serve({ fetch: app.fetch, port }, () => {
+  console.log(`API running on http://localhost:${port}`)
+})
